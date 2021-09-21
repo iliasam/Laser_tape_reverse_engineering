cddd8b816b91d5a96093c63d678e1cbbce09228a
User-PC\User <iliasam@mail.ru>
2021-09-21 20:31:03 +0300

    Added files for phase calculation - not tested.

---
+++ Code/CortexM0/Firmware_phase_calculation/analyse.c
@@ -0,0 +1,117 @@
+#include "stm32f0xx_it.h"
+#include "config_periph.h"
+#include "capture_configure.h"
+#include "math.h"
+#include "analyse.h"
+#include "stdlib.h"
+
+#ifndef M_PI
+#define M_PI            3.1415926535 
+#endif
+
+extern float  APD_temperature;//temperature value in deg
+
+
+#define OMEGA ((2.0 * M_PI * K_COEF) / POINTS_TO_SAMPLE)
+
+float sin_coef = 0.0;
+float cos_coef = 0.0;
+float g_coef = 0.0;//goertzel coefficient
+
+
+void init_goertzel(void)
+{
+  sin_coef = sin(OMEGA);
+  cos_coef = cos(OMEGA);
+  g_coef = 2.0 * cos_coef;
+}
+
+//data goes like data[used] + data[not used] + data[used] ...
+AnalyseResultType goertzel_analyse(uint16_t* data)
+{
+  uint16_t i;
+  float q0, q1, q2;
+  float real, imag;
+  float   scalingFactor = (float)POINTS_TO_SAMPLE / 2.0;
+  AnalyseResultType result;
+  
+  q0=0;
+  q1=0;//n-1
+  q2=0;//n-2
+  
+  for(i=0; i<POINTS_TO_SAMPLE; i++)
+  {
+    q0 = g_coef * q1 - q2 + (float)data[i*2];
+    q2 = q1;
+    q1 = q0;
+  }
+  
+  real = (q1 - q2 * cos_coef)/ scalingFactor;
+  imag = (q2 * sin_coef)/ scalingFactor;
+  result = do_result_conversion(real, imag);
+  return result;
+}
+
+//convert from Re/Im to Amplitude/Phase
+AnalyseResultType do_result_conversion(float real, float imag)
+{
+  AnalyseResultType result;
+  float amplitude = imag * imag + real*real;
+  result.Amplitude = (uint32_t)sqrt(amplitude);
+  
+  float phase = atan2(imag, real);
+  phase = phase * (float)(PHASE_MULT * MAX_ANGLE / 2) / M_PI;
+  result.Phase = (int16_t)(phase);
+  
+  return result;
+}
+
+//phase in deg (0-3599)
+//calculate average phase value
+//data - array of values
+int16_t calculate_avr_phase(int16_t* data, uint16_t length)
+{
+  uint16_t i;
+  uint8_t zero_cross_flag = 0;
+  int32_t tmp_val = 0;
+  
+  //test for zero cross
+  for (i=0; i < length; i++)
+  {
+    if ((data[i] < (ZERO_ANGLE1 * PHASE_MULT)) || (data[i] > (ZERO_ANGLE2 * PHASE_MULT))) zero_cross_flag = 1;
+  }
+  
+  if (zero_cross_flag == 0) //no zero cross 
+  {
+    //simple avarege calculation
+    for (i = 0; i < length; i++) {tmp_val+= (int32_t)data[i];}
+    tmp_val = tmp_val / length;
+    return (uint16_t)tmp_val;
+  }
+  else
+  {
+    //remove zero crossing
+    for (i = 0; i < length; i++) 
+    {
+      tmp_val+= (data[i] > HMAX_ANGLE * PHASE_MULT) ?  ((HMAX_ANGLE + MAX_ANGLE) * PHASE_MULT - data[i]) : (HMAX_ANGLE * PHASE_MULT - data[i]); //0->180, 359->181
+    }
+    tmp_val = tmp_val / length;//positive value
+    //remove additional shift
+    tmp_val = HMAX_ANGLE * PHASE_MULT - tmp_val;//can be negative
+    if (tmp_val < 0) tmp_val = MAX_ANGLE * PHASE_MULT + tmp_val;
+    return tmp_val;
+  }
+}
+
+//calctulase phase_correction value
+
+
+int16_t calculate_correction(uint16_t raw_temperature, uint16_t amplitude, uint8_t apd_voltage)
+{
+  //temperature compensation
+  float correction = 0.226974f * APD_temperature; //it is bad to use APD_temperature here
+  correction+= -0.0049827f * APD_temperature * APD_temperature;
+  return (int16_t)(-correction * PHASE_MULT);
+}
+
+
---
+++ Code/CortexM0/Firmware_phase_calculation/main.c
@@ -0,0 +1,139 @@
+//Firmware for "x-40" laser tape
+//By ILIASAM
+//This program captures data from ADC, calculate phase difference using Goertzel algorithm, send results to UART.
+//All this done sequentially for 3 frequencies.
+//UART baudrate - 256000
+//Phase units - 0.1 deg
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "config_periph.h"
+#include "pll_functions.h"
+#include "capture_configure.h"
+#include "analyse.h"
+#include "stdio.h"
+#include <stdlib.h>
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+uint16_t APD_temperature_raw = 0;//raw temperature value
+float  APD_temperature = 0.0;//temperature value in deg
+float  APD_current_voltage = 80;//value in volts
+
+extern uint8_t uart_disabled_flag;
+
+AnalyseResultType result1;
+AnalyseResultType result2;
+AnalyseResultType result3;
+
+/* Private function prototypes -----------------------------------------------*/
+void do_triple_phase_measurement(void);
+
+/* Private functions ---------------------------------------------------------*/
+
+int main(void)
+{
+  delay_ms(1000);
+  init_all_hardware();
+  init_i2c();
+  delay_ms(200);
+  
+  enable_laser();
+  set_apd_voltage(APD_DEFAULT_VOLTAGE);
+  delay_ms(50);
+  configure_pll();
+  delay_ms(100);
+  
+  pll_set_frequency_1();
+  delay_ms(100);
+  
+  prepare_capture();
+  start_adc_capture();
+  
+  while (1)
+  {
+    //capture_do_single_adc_measurements();//measure temperature
+    do_triple_phase_measurement();
+    
+    //Send results to UART
+    printf("freqA_amp:%d\r\n",  result1.Amplitude);
+    printf("APD temp:%d\r\n",   APD_temperature_raw);
+    printf("Volt:%d\r\n",       (uint16_t)APD_current_voltage);
+    printf("freqA_phase:%d\r\n", result1.Phase);
+    printf("freqB_phase:%d\r\n", result2.Phase);
+    printf("freqC_phase:%d\r\n", result3.Phase);
+    
+    //if auto switch enabled, manual switching is not working
+    //auto_switch_apd_voltage((uint16_t)result1.Amplitude);
+    
+    //Delay_ms(100);
+  }
+}
+
+//Measure phase shifts for three frequencies
+void do_triple_phase_measurement(void)
+{
+    //set freq1
+    pll_set_frequency_1();
+    //dwt_delay(SWITCH_DELAY);
+    delay_ms(1);
+    result1 = do_capture();
+    
+    pll_set_frequency_2();
+    //dwt_delay(SWITCH_DELAY);
+    delay_ms(1);
+    result2 = do_capture();
+    
+    pll_set_frequency_3();
+    //dwt_delay(SWITCH_DELAY);
+    delay_ms(1);
+    result3 = do_capture();
+}
+
+//Process data byte, received by MCU
+void process_rx_data(uint8_t data)
+{
+  switch (data)
+  {
+      case (uint8_t)'E': //Enable laser
+      {
+        enable_laser();
+        break;
+      }
+      case (uint8_t)'D': //Disable laser
+      {
+        disable_laser();
+        break;
+      }
+      case (uint8_t)'H': //Set APD high voltage
+      {
+        set_apd_voltage(APD_HIGH_VOLTAGE);
+        break;
+      }
+      case (uint8_t)'L': //Set APD low voltage
+      {
+        set_apd_voltage(APD_LOW_VOLTAGE);
+        break;
+      }
+    
+    default: break;
+  }
+}
+
+
+int putchar(int c)
+ {
+  if (uart_disabled_flag)
+    return c;
+   
+  // Write a character to the USART
+  USART_SendData(UART_NAME, (uint8_t)c);
+   
+  //Loop until the end of transmission 
+  while(USART_GetFlagStatus(UART_NAME, USART_FLAG_TXE) == RESET)
+  {   
+  }      
+  return c;    
+}
